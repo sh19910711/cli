891af3edf1496132bffc8f88571f7badfd26d295
Seiya Nuta <nuta@seiya.me>
2016-12-05 19:31:02 +0900

    fix serial command

--- makestack/commands/serial.py
+++ makestack/commands/serial.py
@@ -12,18 +12,19 @@ def main(args):
         device_path = detect_device_path()
 
     serial_port = serial.Serial(device_path, 115200, timeout=1)
-    info("listening {}...".format(serial_port))
+    info("listening {}...".format(device_path))
 
     try:
-        try:
-            b = serial_port.read()
-            s = b.decode('ascii')
-        except UnicodeDecodeError:
-            print(repr(b), end='')
-            pass
-        else:
-            print(s, end='')
-            sys.stdout.flush()
+        while True:
+            try:
+                b = serial_port.read()
+                s = b.decode('ascii')
+            except UnicodeDecodeError:
+                print(repr(b), end='')
+                pass
+            else:
+                print(s, end='')
+                sys.stdout.flush()
     except KeyboardInterrupt:
         serial_port.close()
         sys.exit()
