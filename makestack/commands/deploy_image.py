e18082f05c03abb961ff4808aea073b6bb8fff6c
Seiya Nuta <nuta@seiya.me>
2016-12-05 19:30:07 +0900

    fix deploy_image

--- makestack/commands/deploy_image.py
+++ makestack/commands/deploy_image.py
@@ -4,9 +4,10 @@ from makestack.helpers import error
 
 
 def main(args):
+    image_file = os.path.abspath(args.image)
     appdir.chdir_to_app_dir(args.appdir)
     app_name = appdir.get_current_app_name()
 
     api.invoke('POST', '/apps/{}/deployments'.format(app_name),
-               data={ 'comment': args.comment },
-               files={ 'image': open(args.image, 'rb') })
+               params={ 'comment': args.comment },
+               files={ 'image': open(image_file, 'rb') })
