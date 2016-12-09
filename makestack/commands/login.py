527586f74d152335c63e9edf5a8348670633c861
Seiya Nuta <nuta@seiya.me>
2016-12-09 11:09:53 +0900

    login: support MAKESTACK_SERVER_URL, MAKESTACK_USERNAME, and MAKESTACK_PASSWORD

--- makestack/commands/login.py
+++ makestack/commands/login.py
@@ -1,14 +1,28 @@
+import os
 import getpass
 from makestack import api
+
+
+def get_env_or_ask(env, ask):
+    v = os.environ.get(env)
+
+    if v is None:
+        return ask()
+    else:
+        return v
 
 
 def main(args):
     DEFAULT_URL = 'https://makestack.io'
 
-    url = input('server URL [{}]: '.format(DEFAULT_URL))
+    url = get_env_or_ask("MAKESTACK_SERVER_URL",
+                         lambda: input('server URL [{}]: '.format(DEFAULT_URL)))
     if url == '':
         url = DEFAULT_URL
 
-    username = input('username: ')
-    password = getpass.getpass('password: ')
+    username = get_env_or_ask("MAKESTACK_USERNAME",
+                              lambda: input('username: '))
+    password = get_env_or_ask("MAKESTACK_PASSWORD",
+                              lambda: getpass.getpass('password: '))
+
     api.login(url, username, password)
