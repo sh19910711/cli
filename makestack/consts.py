8669c096e2fbc4e89d4402b9c5c51692cc5d03f6
Seiya Nuta <nuta@seiya.me>
2016-12-09 11:09:30 +0900

    support MAKESTACK_CONFIG_DIR

--- makestack/consts.py
+++ makestack/consts.py
@@ -1,6 +1,6 @@
 import os
 
 
-CONFIG_DIR_PATH = os.path.expanduser('~/.makestack')
+CONFIG_DIR_PATH = os.environ.get("MAKESTACK_CONFIG_DIR", os.path.expanduser('~/.makestack'))
 CREDENTIAL_YAML_PATH = os.path.join(CONFIG_DIR_PATH,
                                     'credentials.yaml')
