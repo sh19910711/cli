79107d5da8c0cc9f2492bc8644110e50088983bc
Seiya Nuta <nuta@seiya.me>
2016-12-12 13:25:28 +0900

    esp8266: update the firmware

--- makestack/boards/esp8266.py
+++ makestack/boards/esp8266.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def get_firmware_url():
-    return 'https://github.com/makestack/esp8266-firmware/releases/download/v0.1.0/firmware.bin'
+    return 'https://github.com/makestack/esp8266-firmware/releases/download/v0.3.0/firmware.bin'
 
 
 def install(serial, firmware_path):
