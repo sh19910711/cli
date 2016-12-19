9dadb427a28983736c0e6febec48f45eecf2725f
Seiya Nuta <nuta@seiya.me>
2016-12-19 22:47:53 +0900

    fix tests

--- test/test_add_device.py
+++ test/test_add_device.py
@@ -2,6 +2,5 @@ from helpers import *
 
 
 def test_add_device(app):
-    assert rails("Device.find_by_name('x-wing').app.name") == ""
     run(["add-device", "x-wing"])
     assert rails("Device.find_by_name('x-wing').app.name") == app
--- test/test_deploy.py
+++ test/test_deploy.py
@@ -3,6 +3,7 @@ from helpers import *
 
 def test_deploy(app):
     run(["config"])
+    run(["add-device", "x-wing"])
     run(["deploy"])
     assert rails("Deployment.all.first.app.name") == app
     assert rails("Build.all.first.app.name") == app
